8bc307ffb16768d64d389b28f4d53afde87662b6
Prisana Cheng <chengprisana@gmail.com>
2022-06-27 15:09:52 -0500

    added dogs

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,12 +1,20 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2206-FTB-ET-WEB-FT';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
+         try {
+          const response = await fetch(`${APIURL}/players`);
+          const result = await response.json();
+          if (result.error) throw result.error;
+          return result.data.players;
+        } catch (err) {
+          console.error('Uh oh, trouble fetching players!', err);
+        }
+      };
 
-};
 
 export const fetchSinglePlayer = async (playerId) => {
 
